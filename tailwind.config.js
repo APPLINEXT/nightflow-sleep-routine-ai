d3e229217c9f1d31f93563cc1aa785e15b6ff40d
APPLINEXT <eliasloopsnwar@gmail.com>
2025-06-21 17:35:08 +0200

    tu connais

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#f0f0f0",
+        border: "#cccccc",
+      },
+    },
+  },
+  plugins: [],
+};
